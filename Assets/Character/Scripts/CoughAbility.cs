bbec7d59b108e9f2ff65604e9892851f571120e9
Flaf234 <costi.berindei03@e-uvt.ro>
2026-01-31 22:07:25 +0200

    cough ability server sided hopefully

--- Assets/Character/Scripts/CoughAbility.cs
+++ Assets/Character/Scripts/CoughAbility.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 using System.Collections;
-
-public class CoughAbility : MonoBehaviour
+using Unity.Netcode;
+using System.Runtime.CompilerServices;
+public class CoughAbility : NetworkBehaviour
 {
     [Header("Settings")]
     public float minRange = 0.5f;
@@ -10,11 +11,13 @@ public class CoughAbility : MonoBehaviour
     public float cooldownDuration = 5f;
     public LayerMask targetLayer; 
 
+    public NetworkVariable<float> currentCooldown = new NetworkVariable<float>(0f, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+    private float lastServerTime;
     [Header("Visual Indicator")]
     [Tooltip("Use a circle transform with scale = 2 * minRange to indicate the range of the cough.")]
     public Transform rangeIndicator; 
 
-    private float currentCooldown = 0f;
+    //private float currentCooldown = 0f;
     private float currentChargeTime = 0f;
     private bool _isCharging = false;
     public bool isCharging
@@ -30,12 +33,31 @@ public class CoughAbility : MonoBehaviour
             }
         }
     }
+    public override void OnNetworkSpawn()
+    {
+        if(IsServer) lastServerTime = Time.time;
+    }
+    // void Update()
+    // {
+    //     //if(currentCooldown > 0f) currentCooldown -= Time.deltaTime;
+    //     if (IsServer)
+    //     {
+    //         float now = Time.time;
+    //         float dt =  now - lastServerTime;
+    //         lastServerTime = now;
+            
+    //         if(CooldownRemaining.Value > 0f)
+    //             CooldownRemaining.Value = Mathf.Max(0f, CooldownRemaining.Value - dt);
+    //     }
+    // }
+
 
     void Update()
     {
-        if (currentCooldown > 0)
+        if(IsServer){
+        if (currentCooldown.Value > 0)
         {
-            currentCooldown -= Time.deltaTime;
+            currentCooldown.Value -= Time.deltaTime;
             return;
         }
         if (_isCharging)
@@ -50,22 +72,29 @@ public class CoughAbility : MonoBehaviour
                 rangeIndicator.localScale = Vector3.one * (currentRadius * 2f); 
             }
         }
+        }
     }
 
-    public void BeginCharge()
+  
+  
+     public void BeginCharge()
     {
-        if(currentCooldown > 0) return;
+        if (!IsOwner) return;
+        //TryInfectServerRpc();
+        if(currentCooldown.Value > 0) return;
         if(_isCharging) return;
         _isCharging = true;
         currentChargeTime = 0f;
     }
     public void ReleaseCharge()
     {
+        if (!IsOwner) return;
         if(!_isCharging) return;
-        FireCough();
+        FireCoughServerRpc();
     }
 
-    void FireCough()
+    [ServerRpc]
+    void FireCoughServerRpc(ServerRpcParams rpcParams = default)
     {
         _isCharging = false;
         if (rangeIndicator != null) rangeIndicator.gameObject.SetActive(false);
@@ -80,11 +109,14 @@ public class CoughAbility : MonoBehaviour
         {
 
             if (hit.gameObject == gameObject) continue;
-
-            Debug.Log($"Coughed on: {hit.name}");
-            
+            NpcInfected npcInfected = hit.GetComponentInParent<NpcInfected>();
+            if (npcInfected != null)
+            {
+                npcInfected.InfectServer();
+                Debug.Log($"Coughed on: {hit.name}");
+            }
         }
-        currentCooldown = cooldownDuration;
+        currentCooldown.Value = cooldownDuration;
     }
     void OnDrawGizmosSelected()
     {
